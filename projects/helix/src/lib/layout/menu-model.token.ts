29d1a34ef3ce97d53256161fa100faa6d6080dd6
Jérôme Kaufmann <kaufmann.jerome@live.de>
2026-06-20 06:29:01 +0200

    feat(app-layout): remove HELIX_MENU_MODEL fallback, prioritize route data for menu resolution

--- projects/helix/src/lib/layout/menu-model.token.ts
+++
@@ -1,6 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import type { MenuItem } from 'primeng/api';
-
-export const HELIX_MENU_MODEL = new InjectionToken<MenuItem[]>('HELIX_MENU_MODEL', {
-  factory: () => [],
-});
